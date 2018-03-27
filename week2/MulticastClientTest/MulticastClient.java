742cc07b288c533367440f51da3b602d433a82f4
Joowon <joowon7508@gmail.com>
2018-03-27 17:29:04 +0900

    some test files for RMI and Multicast

---
+++ week2/MulticastClientTest/MulticastClient.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+public class MulticastClient {
+
+    final static String INET_ADDR = "";
+    final static int PORT = 888;
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getByName(INET_ADDR);
+
+        // create a buffer of bytes for receiving
+        byte[] buf = new byte[256];
+
+        try (MulticastSocket clientSocket = new MulticastSocket(PORT)) {
+
+            clientSocket.joinGroup(address);
+
+            while (true) {
+                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
+                clientSocket.receive(msgPacket);
+
+                String msg = new String(buf, 0, buf.length);
+                System.out.println("Socket received msg: " + msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ week2/MulticastServerTest/MulticastServer.java
@@ -0,0 +1,26 @@
+import java.net.*;
+
+public class MulticastServer {
+
+    final static String INET_ADDR  = "";
+    final static int PORT = 8888;
+
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        InetAddress addr = InetAddress.getByName(INET_ADDR);
+
+        try (DatagramSocket serverSocket = new DatagramSocket()) {
+            for (int i = 0; i < 5; i++) {
+                String msg = "Sent message no " + i;
+
+                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
+                        msg.getBytes().length, addr, PORT);
+                serverSocket.send(msgPacket);
+
+                System.out.println("Server sent packet with msg : " + msg);
+                Thread.sleep(500);
+            }
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
